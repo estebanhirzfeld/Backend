5d17536fc642bab8aa8e1f426a5ff826d9de85f1
estebanhirzfeld <estebanhirzfeld@gmail.com>
2022-03-03 16:09:54 -0300

    clase3

--- clase3/JavascriptAsincronico.js
+++ clase3/JavascriptAsincronico.js
@@ -1,37 +1,39 @@
-// let TextReceiver = (text,processor) => {    
-// }
+//Texto de Wake me up - Avicii
+let texto = "So wake me up when it's all over When I'm wiser and I'm older All this time I was finding myself, and I Didn't know I was lost";
 
-// let TextDivider = () => {}
-
-// let TextShow = (seconds = 1000) => {}
-
-// let ProccessFinished = () => {
-//     console.log("Proccess Complete");
-// }
-
-
-const mostrarLetras = (palabra,cb) =>{
+// Recibe un texto y una funcion, la funcion se ejecuta en el setTimeout cuando el proceso finaliza 
+// El texto se divide en un array de palabras y se recorre con un setInterval
+let TextReceiver = (text, processor) => {
+    textSplittedArray = TextDivider(text);
     let i = 0;
-    const recorrerPalabra = ()=>{
-        if(i<palabra.length){
-            console.table(palabra[i]);
+    const recorrerPalabra = () => {
+        if (i < textSplittedArray.length) {
+            console.log(textSplittedArray[i]);
             i++;
-        }else{
+        } else {
             clearInterval(timer);
-            cb();
+            processor();
         }
     }
-    const timer = setInterval(recorrerPalabra,1000);
+    const timer = setInterval(recorrerPalabra, 1000);
 }
 
-const funcionFinalizado = () => console.log("FINALIZADO EL PROCESO");
+// Divide un texto en un array de palabras
+let TextDivider = (text) => {
+    return text.split(" ");
+}
 
-setTimeout(()=>{
-    mostrarLetras('HEMOS',funcionFinalizado)
-},0);
-setTimeout(()=>{
-    mostrarLetras('FINALIZADO',funcionFinalizado)
-},250);
-setTimeout(()=>{
-    mostrarLetras('POR HOY',funcionFinalizado)
-},500);
+// Ejecuta la funcion TextReceiver con el texto y la funcion ProccessFinished
+let TextShow = (seconds,text) => {
+    setTimeout(()=>{
+        TextReceiver(text,ProccessFinished)
+    },seconds);
+}
+
+// Funcion que se ejecuta cuando el proceso finaliza
+let ProccessFinished = () => {
+    console.log("Proccess Complete");
+    console.log("Total Words: " + textSplittedArray.length);
+}
+
+TextShow(1000,texto);
